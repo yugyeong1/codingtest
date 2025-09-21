7f89795e8088a278086086b7cc85c5f6aca2172c
yugyeong1 <dbrud1032@gmail.com>
2025-09-21 18:35:41 +0900

    bfs
    bfs 연습

---
+++ bfs/1/bfs_test3.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+n = 5
+m = 6
+
+graph = [ 
+            [1, 0, 1, 0, 1, 0],
+            [1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1]
+        ]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            # 범위를 벗어났음
+            if nx < 0 or nx >= n or ny <0 or ny >= m:
+                continue
+            
+            # 벽이므로 continue
+            if graph[nx][ny] == 0:
+                continue
+            
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+                
+    return graph[n-1][m-1]
+
+
+result = bfs(0, 0)
+print(result)
---
+++ bfs/2/test4.py
@@ -0,0 +1,56 @@
+
+# N×N 땅이 주어진다. 각 칸에는 높이가 적혀 있다.
+# 비가 와서 특정 높이 h 이하의 지역은 잠긴다.
+# 잠기지 않은 지역들 중에서 연결된 영역의 개수를 구하라.
+# 출력: 모든 h에 대해 가능한 영역 개수 중 최대값
+from collections import deque
+
+n = 5
+area = [
+            [6, 8, 2, 6, 2], 
+            [3, 2, 3, 4, 6],
+            [6, 7, 3, 3, 2],
+            [7, 2, 5, 3, 6],
+            [8, 9, 5, 2, 7]
+        ]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y, height):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0  or nx >= n or ny < 0 or ny >= n:
+                continue
+            if visited[nx][ny] == True:
+                continue
+            if visited[nx][ny] == False and area[nx][ny] > height:
+                visited[nx][ny] = True
+                queue.append((nx, ny))
+    
+
+max_height = max(max(row) for row in area)
+result = 0
+for height in range(max_height + 1):
+    visited = [ [False] * n for i in range (n) ]
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            # 영역 한 부분이 잠기지 않고 방문한 적이 없을 경우
+            if area[i][j] > height and visited[i][j] == False:
+                bfs(i, j, height)
+                count += 1
+    result = max(result, count)
+    
+print(result)
+                
